e5d8d594dd7923216d301706dbc17dcbddc94567
Hauger987 <hhauger@muhlenberg.edu>
2024-05-06 14:31:38 -0400

    key dropping added

--- My Project/main/slideBoss.go
+++ My Project/main/slideBoss.go
@@ -162,3 +162,22 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "key_factory"
+  type: "factory"
+  data: "prototype: \"\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
